5e64e05ca8102ea3b5bcea3f7d94300aa7e73e13
Trevor Pering <peringknife@google.com>
2019-01-18 16:15:26 -0800

    Updating with more concise syntax

--- testing/test_base.sh
+++ testing/test_base.sh
@@ -50,14 +50,15 @@ cp misc/system_muddy.conf local/system.conf
 device_traffic="tcpdump -en -r inst/run-port-01/scans/monitor.pcap port 47808"
 device_bcast="$device_traffic and ether broadcast"
 device_ucast="$device_traffic and ether dst 9a:02:57:1e:8f:02"
-device_xcast="$device_traffic and ether dst 9a:02:57:1e:8f:03 or ether src 9a:02:57:1e:8f:03"
+device_xcast="$device_traffic and ether host 9a:02:57:1e:8f:03"
 cntrlr_traffic="tcpdump -en -r inst/run-port-02/scans/monitor.pcap port 47808"
 cntrlr_bcast="$cntrlr_traffic and ether broadcast"
 cntrlr_ucast="$cntrlr_traffic and ether dst 9a:02:57:1e:8f:01"
-cntrlr_xcast="$cntrlr_traffic and ether dst 9a:02:57:1e:8f:03 or ether src 9a:02:57:1e:8f:03"
+cntrlr_xcast="$cntrlr_traffic and ether host 9a:02:57:1e:8f:03"
 
-function summarize_results {
+function test_mud {
     type=$1
+    $cmdrun -s device_specs=misc/device_specs_bacnet_$type.json
     bcast=$($device_bcast | wc -l)
     ucast=$($device_ucast | wc -l)
     xcast=$($device_xcast | wc -l)
@@ -68,22 +69,11 @@ function summarize_results {
     echo cntrlr $type $(($bcast > 2)) $(($ucast > 2)) $(($xcast > 0)) | tee -a $TEST_RESULTS
 }
 
-$cmdrun -s
-summarize_results open
-
-$cmdrun -s device_specs=misc/device_specs_bacnet.json
-summarize_results base
-
-$cmdrun -s device_specs=misc/device_specs_bacnet_todev.json
-summarize_results todev
-
-$cmdrun -s device_specs=misc/device_specs_bacnet_frdev.json
-summarize_results frdev
-
-$cmdrun -s device_specs=misc/device_specs_bacnet_disabled.json
-summarize_results none
-
-$cmdrun -s device_specs=misc/device_specs_bacnet_star.json
-summarize_results star
+test_mud open
+test_mud base
+test_mud todev
+test_mud frdev
+test_mud none
+test_mud star
 
 echo Done with tests | tee -a $TEST_RESULTS
