eb8991537228f018a6c2a89b363041ce536062c3
Trevor Pering <peringknife@google.com>
2019-02-11 12:34:03 -0800

    Include test_stack.out

--- testing/test_stack.sh
+++ testing/test_stack.sh
@@ -2,6 +2,8 @@
 
 source testing/test_preamble.sh
 
+out_base=/tmp/daq-test_stack
+
 echo Stacking Tests >> $TEST_RESULTS
 
 bin/setup_stack
@@ -15,13 +17,24 @@ function test_pair {
     src=$1
     dst=$2
     
-    results=$(docker exec -ti daq-faux-$src ping -c 10 192.168.0.$dst | fgrep time= | wc -l)
-    echo Ping from $src $dst returned $results | tee -a $TEST_RESULTS
+    out_file=$out_base-$src-$dst
+    host=daq-faux-$src
+    cmd="ping -c 10 192.168.0.$dst"
+    echo $host: $cmd
+    echo -n $host: $cmd\ > $out_file
+    docker exec -ti $host $cmd | fgrep time= | wc -l >> $out_file &
 }
 
+rm -f $out_base-*
 test_pair 1 2
 test_pair 1 3
 test_pair 2 1
 test_pair 2 3
 test_pair 3 1
 test_pair 3 2
+
+wait
+
+cat $out_base-* | tee -a $TEST_RESULTS
+
+echo Done with stack test. | tee -a $TEST_RESULTS
