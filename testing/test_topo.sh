f52bdc483ebc4ae77c49d43cf4810535f908a2b9
Trevor Pering <peringknife@google.com>
2019-02-10 16:32:52 -0800

    Do proper tcp checking

--- testing/test_topo.sh
+++ testing/test_topo.sh
@@ -30,7 +30,7 @@ function generate {
   iface_names=
   for iface in $(seq 1 $faux_num); do
       iface_names=${iface_names},faux-$iface
-      echo autostart cmd/faux $iface discover telnet >> $topostartup
+      echo autostart cmd/faux $iface discover >> $topostartup
   done
   echo intf_names=${iface_names#,} >> local/system.conf
 
@@ -40,76 +40,88 @@ function generate {
   echo site_description=\"$type with $devices devices\" >> local/system.conf
   echo device_specs=misc/device_specs_topo_$type.json >> local/system.conf
   echo test_config=inst/runtime_conf/ >> local/system.conf
+  # Don't use default monitor scan to get both src/dst traffic.
   echo monitor_scan_sec=0 >> local/system.conf
 }
 
 MAC_BASE=9a:02:57:1e:8f
 
+function check_setup {
+    src_dev=$1
+    conf_dir=inst/runtime_conf/port-$(printf %02d $src_dev)
+    cmd_file=$conf_dir/ping_runtime.sh
+    echo $cmd_file
+    if [ -f $cmd_file ]; then
+        return
+    fi
+    mkdir -p $conf_dir
+
+    cat >> $cmd_file <<EOF
+timeout 30 tcpdump -eni \$HOSTNAME-eth0 -w /tmp/eth0.pcap || true
+function test_bacnet {
+    bacnet_base="tcpdump -en -r /tmp/eth0.pcap port 47808"
+    echo \$((\$(\$bacnet_base and \$@ | wc -l ) > 0))
+}
+function test_tcp {
+    src_mac=$MAC_BASE:$(printf %02x $src_dev)
+    dst_mac=$MAC_BASE:\$(printf %02x \$1)
+    # Check for TCP ACKs, since that means the network is allowing it.
+    tcp_base="tcpdump -en -r /tmp/eth0.pcap tcp and ether dst \$src_mac"
+    filter="ether src \$dst_mac and src port \$2"
+    echo \$((\$(\$tcp_base and \$filter | wc -l ) > 0))
+}
+EOF
+}
+
 function check_bacnet {
-    at_dev=$(printf %02d $1)
-    ex_dev=$(printf %02d $2)
+    src_dev=$(printf %02d $1)
+    dst_dev=$(printf %02d $2)
     shift
     shift
     expected="$*"
 
-    at_mac=$MAC_BASE:$(printf %02x $at_dev)
-    ex_mac=$MAC_BASE:$(printf %02x $ex_dev)
+    src_mac=$MAC_BASE:$(printf %02x $src_dev)
+    dst_mac=$MAC_BASE:$(printf %02x $dst_dev)
 
-    conf_dir=inst/runtime_conf/port-$at_dev
-    mkdir -p $conf_dir
-    cmd_file=$conf_dir/ping_runtime.sh
-
-    tcp_base="tcpdump -en -r /tmp/eth0.pcap port 47808"
-
-    if [ ! -f $conf_dir/.test_bacnet ]; then
-        touch $conf_dir/.test_bacnet
-        cat >> $cmd_file <<EOF
-sleep 30 # For startup race-conditions.
-timeout 30 tcpdump -eni \$HOSTNAME-eth0 -w /tmp/eth0.pcap || true
-function testit {
-    echo \$((\$($tcp_base and \$@ | wc -l ) > 0))
-}
-EOF
-    fi
+    cmd_file=$(check_setup $src_dev)
 
     cat >> $cmd_file <<EOF
-ether_dst=\$(testit ether dst $ex_mac)
-ether_not=\$(testit not ether src $at_mac and not ether dst $at_mac)
-bcast_src=\$(testit ether broadcast and ether src $at_mac)
-bcast_oth=\$(testit ether broadcast and not ether src $at_mac)
+ether_dst=\$(test_bacnet ether dst $dst_mac)
+ether_not=\$(test_bacnet not ether src $src_mac and not ether dst $src_mac)
+bcast_src=\$(test_bacnet ether broadcast and ether src $src_mac)
+bcast_oth=\$(test_bacnet ether broadcast and not ether src $src_mac)
 result="\$ether_dst \$ether_not \$bcast_src \$bcast_oth"
-echo check_bacnet $at_dev $ex_dev \$result | tee -a $bacnet_file
+echo check_bacnet $src_dev $dst_dev \$result | tee -a $bacnet_file
 [ -z "$expected" -o "$expected" == "\$result" ] || (echo \$result != $expected && false)
 EOF
 }
 
 function check_socket {
-    from_dev=$1
-    to_dev=$2
+    src_dev=$1
+    dst_dev=$2
     shift
     shift
     expected="$*"
 
-    to_host=daq-faux-$(printf %d $to_dev)
+    src_host=daq-faux-$(printf %d $src_dev)
+    dst_host=daq-faux-$(printf %d $dst_dev)
 
-    conf_dir=inst/runtime_conf/port-$(printf %02d $from_dev)
+    faux_dir=inst/runtime_conf/$src_host
+    mkdir -p $faux_dir
+    conf_dir=inst/runtime_conf/port-$(printf %02d $src_dev)
     mkdir -p $conf_dir
-    cmd_file=$conf_dir/ping_runtime.sh
 
-    if [ ! -f $conf_dir/.test_socket ]; then
-        touch $conf_dir/.test_socket
-        cat >> $cmd_file <<EOF
-function tcp_port {
-    (timeout 10 nc $to_host \$1 2>/dev/null || echo Failed-$to_dev) | xargs
-}
-EOF
-    fi
+    start_faux=$faux_dir/start_faux.sh
+    for port in 23 443; do
+        echo "(while sleep 5; do timeout 5 nc $dst_host $port; done) &" >> $start_faux
+    done
 
+    cmd_file=$(check_setup $src_dev)
     cat >> $cmd_file <<EOF
-telnet=\$(tcp_port 23)
-https=\$(tcp_port 443)
+telnet=\$(test_tcp $dst_dev 23)
+https=\$(test_tcp $dst_dev 443)
 result="\$telnet \$https"
-echo check_socket $from_dev $to_dev \$result >> $socket_file
+echo check_socket $src_dev $dst_dev \$result >> $socket_file
 [ -z "$expected" -o "$expected" == "\$result" ] || (echo \$result != $expected && false)
 EOF
 }
@@ -127,20 +139,20 @@ function run_test {
 }
 
 generate open 3
-check_socket 01 02 daq-faux-2 Failed-02
+check_socket 01 02 1 1
 check_bacnet 01 02 1 1 1 1
 check_bacnet 02 03 1 1 1 1
 check_bacnet 03 01 1 1 1 1
 run_test
 
 generate minimal 3
-check_socket 01 02 Failed-02 Failed-02
 check_bacnet 01 02 1 1 1 1
 check_bacnet 02 03 0 1 1 1
 check_bacnet 03 01 1 1 1 1
 run_test
 
 generate commissioning 4
+check_socket 01 02 0 0
 check_bacnet 01 02 1 1 1 1
 check_bacnet 01 04 0 1 1 1
 check_bacnet 02 03 0 1 1 1
