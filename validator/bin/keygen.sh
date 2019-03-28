fb7368ccb0bf692898a314d370e67ee6ba9c9f1c
Trevor Pering <peringknife@google.com>
2019-03-28 11:19:16 -0700

    Adding pkcs8 output

--- validator/bin/keygen.sh
+++ validator/bin/keygen.sh
@@ -8,3 +8,4 @@ fi
 cd $1
 
 openssl req -x509 -nodes -newkey rsa:2048 -keyout rsa_private.pem -days 1000000 -out rsa_public.pem -subj "/CN=unused"
+openssl pkcs8 -topk8 -inform PEM -outform DER -in rsa_private.pem -nocrypt > rsa_private.pkcs8
