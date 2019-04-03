16cadbdd6dade0fb03948803bd6a10ef578aa4c3
Edward Williams <edward.williams@redstone.com>
2019-04-03 17:38:14 +0100

    added in packet received count

--- subset/network/network_min_send.py
+++ subset/network/network_min_send.py
@@ -22,11 +22,40 @@ def shell_command_fb(command, wait_time, terminate_flag):
     if len(text) > 0:
         return text
 
+pointer_list_line_end = []
+
+def find_char_pointer(value, find_text, pointer_list):
+    pointer = 0
+    while pointer != -1:
+        pointer = value.find(find_text,pointer)
+        if pointer == -1:
+            break
+        else:
+            #print('pointer:' , pointer)
+            pointer_list.append(pointer)
+        pointer += 1
+
+packet_request_list = []
+
+def cut_lines_to_list(request_list,pointer_list):
+    last_point = 0
+    for point in pointer_list:
+        request_list.append(shell_result[last_point:point])
+        last_point = point + 1
+        #print(point)
+
 shell_result = shell_command_fb(tcpdump_display_all_packets, 0, False)
+
+find_char_pointer(shell_result, '\n', pointer_list_line_end)
+
+cut_lines_to_list(packet_request_list,pointer_list_line_end)
+
+packets_received = len(packet_request_list)
 
 file_open = open(report_filename, 'w')
 
 if len(shell_result) > 40:
+    file_open.write('packets_received:' + str(packets_received)  + '\n')
     file_open.write('network.min_send=true\n')
 else:
     file_open.write('network.min_send=false\n')
