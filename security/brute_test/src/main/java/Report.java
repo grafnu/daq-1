c7264faebce12448ce027f41cd6ffa6b21e67019
Ketan Tak <45993431+ktredstone@users.noreply.github.com>
2019-05-02 09:54:50 +0100

    Add files via upload

---
+++ security/brute_test/src/main/java/Report.java
@@ -0,0 +1,53 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Report {
+	String report = "Brute Test *";
+	String reportFilename = "tmp/report.txt";
+	
+	
+	public void addText(String text) {
+		report += text;
+	}
+	
+	public void printReport() {
+		System.out.println(report);
+	}
+	
+	public void writeReport(String protocol) {
+		switch(protocol) {
+		case "telnet":
+			reportFilename = "tmp/telnetReport.txt";
+			break;
+		
+		case "ssh":
+			reportFilename = "tmp/sshReport.txt";
+			break;
+		}
+		 try {
+			 String[] directory = reportFilename.split("/");
+			 String[] lines = report.split("\\*");
+
+		      File dir = new File(directory[directory.length - 2]);
+		      if (!dir.exists()) dir.mkdirs();
+			 
+		     BufferedWriter writer = new BufferedWriter(new FileWriter(reportFilename));
+		      
+		     for(String tmpLine : lines) {
+		    	 writer.write(tmpLine);
+		    	 writer.newLine();
+		     }
+		     
+		     writer.close();
+		 	}
+		 catch (IOException e) {
+			 System.err.println("could not write report");
+			 System.out.println(e);
+		 }
+     
+		}
+	
+
+}
