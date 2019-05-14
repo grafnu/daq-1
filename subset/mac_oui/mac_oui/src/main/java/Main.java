4787c6e6e91e9ff424d6960efb7102366b818c02
root <ketan.tak@redstone.com>
2019-05-14 16:10:32 +0100

    Edited project files to suit daq

--- subset/mac_oui/mac_oui/src/main/java/Main.java
+++ subset/mac_oui/mac_oui/src/main/java/Main.java
@@ -2,15 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-	
-		  
-//		if (args.length != 1) {
-//	      throw new IllegalArgumentException(
-//	          "Usage: target_mac");
-//	    }
+	if (args.length != 1) {
+	      throw new IllegalArgumentException(
+	          "Usage: target_mac");
+	    }
 		
-//	String macAddress = args[0];
-	String macAddress = "01:1a:0d:we:09:28";
+	String macAddress = args[0];
+//	String macAddress = "01:1a:0d:we:09:28";
 	System.out.println("Main Started...");
 	RetrieveList setupTest = new RetrieveList(macAddress);
 	}
