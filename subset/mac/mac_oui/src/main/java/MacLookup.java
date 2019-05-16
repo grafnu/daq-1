143781779c1d13e1d79bbef78453b53b29bdfa01
root <ketan.tak@redstone.com>
2019-05-16 10:53:43 +0100

    connection.mac_oui test now runs within daq with a successful ammend to the reports file after PASS/FAIL

--- subset/mac/mac_oui/src/main/java/MacLookup.java
+++ subset/mac/mac_oui/src/main/java/MacLookup.java
@@ -4,19 +4,21 @@ public class MacLookup implements Runnable{
 	String macAddress;
 	Map macDevices;
 	ReportHandler reportHandler = new ReportHandler();
-	
+
 	MacLookup(Map macDevices, String macAddress){
 		this.macDevices = macDevices;
 		this.macAddress = macAddress;
 	}
-	
+
 	public void startTest(){
 		System.out.println("Starting connection.mac_oui test...");
 		String splicedMac = macAddress.replace(":", "");
 		String formattedMac = splicedMac.substring(0, 6).toUpperCase();
+                System.out.println(formattedMac);
 		try{
 		String manufacturer = macDevices.get(formattedMac).toString();
 			if(manufacturer.equals(null)) {
+				System.out.println("Could not find manufacturer");
 				reportHandler.addText("connection.mac_oui FAILED *");
 				reportHandler.addText("could not find device manufacturer *");
 				reportHandler.writeReport();
@@ -29,6 +31,7 @@ public class MacLookup implements Runnable{
 			}
 		}
 		catch(NullPointerException e) {
+			System.out.println(e + " could not find the manufacturer");
 			reportHandler.addText("connection.mac_oui FAILED *");
 			reportHandler.addText("could not find device manufacturer *");
 			reportHandler.writeReport();
