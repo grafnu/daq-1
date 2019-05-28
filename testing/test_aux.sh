b5fe8f7d5488c38566fd852b9f8521410f4e2c58
joelureellanah <joel.ureellanah@redstone.com>
2019-05-28 15:19:18 +0100

    issue 132 - moved bacext test inside test_aux.sh and added new line at the end of the report

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -15,6 +15,30 @@ echo Lint checks | tee -a $TEST_RESULTS
 cmd/inbuild skip
 echo cmd/inbuild exit code $? | tee -a $TEST_RESULTS
 
+# bacext testing
+echo Running bacext test... | tee -a $TEST_RESULTS
+cp misc/host_tests.conf local/local_tests.conf
+cat <<EOF >>local/local_tests.conf
+include subset/bacnet/build.conf
+EOF
+
+cp misc/system_base.conf local/system.conf
+cat <<EOF >>local/system.conf
+startup_faux_opts=bacnet
+host_tests=local/local_tests.conf
+EOF
+cat <<EOF > local/site/module_config.json
+{
+  "modules": {
+    "bacext": {
+      "enabled": true
+    }
+  }
+}
+EOF
+cmd/run -s -b
+cat inst/run-port-01/nodes/bacext01/tmp/report.txt | tee -a $TEST_RESULTS
+
 echo Extended tests | tee -a $TEST_RESULTS
 cp misc/system_multi.conf local/system.conf
 cat <<EOF >> local/system.conf
