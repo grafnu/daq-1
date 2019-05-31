4fbc7cd32d9602447449ee9b9c3459061aab88f2
Edward Williams <edward.williams@redstone.com>
2019-05-31 15:38:53 +0100

    eth_interface

--- subset/network/debug_generate_capture.py
+++ subset/network/debug_generate_capture.py
@@ -5,11 +5,12 @@ import sys
 arguments = sys.argv
 
 capture_time = int(arguments[1])
+eth_interface = arguments[2]
 
 cap_pcap_file = 'capture.pcap'
 header_cmd = 'tcpdump '
 
-tcpdump_capture_unlimited_byte_packets = header_cmd + '-i enxb49cdff33ad9 -s0 -w ' + cap_pcap_file
+tcpdump_capture_unlimited_byte_packets = header_cmd + '-i ' + eth_interface  + ' -s0 -w ' + cap_pcap_file
 
 def shell_command_without_result(command, wait_time, terminate_flag):
     process = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
