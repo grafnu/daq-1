affaae23eb690b06131cae7972d4c3abcfd84c99
Trevor Pering <peringknife@google.com>
2019-06-12 08:59:13 -0700

    lizard updates!

--- misc/runtime_configs/long_wait/port-01/ping_runtime.sh
+++ misc/runtime_configs/long_wait/port-01/ping_runtime.sh
@@ -1,3 +1,7 @@
+# Used for testing of device and type customization mappings in test_aux.sh
+cp /config/device/snake.txt /tmp/
+cp /config/type/lizard.txt /tmp/
+
 # Sleep for 5 minutes to let long-lease dhcp discovery happen.
 sleep_time=$((5*60))
 echo Sleeping for ${sleep_time}s to wait for DHCP.
