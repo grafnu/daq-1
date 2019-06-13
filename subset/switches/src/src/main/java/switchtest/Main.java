d4e6a1599e5f9f094d5317611b92f926840a71d4
Edward Williams <edward.williams@redstone.com>
2019-06-13 11:17:39 +0100

    google formatter

---
+++ subset/switches/src/src/main/java/switchtest/Main.java
@@ -0,0 +1,23 @@
+package switchtest;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    if (args.length != 3) {
+      throw new IllegalArgumentException("Expected ipAddress && port && supportPOE as arguments");
+    }
+
+    String ipAddress = args[0];
+
+    int interfacePort = Integer.parseInt(args[1]);
+
+    boolean supportsPOE = (args[2].equals("true")) ? true : false;
+
+    SwitchInterrogator switchInterrogator =
+        new SwitchInterrogator(ipAddress, interfacePort, supportsPOE);
+
+    Thread switchInterrogatorThread = new Thread(switchInterrogator);
+    switchInterrogatorThread.start();
+  }
+}
