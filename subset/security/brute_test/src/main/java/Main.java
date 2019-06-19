b65f85baa3f2c867e3899369db54c3b25b57e653
Ketan Tak <ketan.tak@redstone.com>
2019-06-19 16:36:17 +0100

    configured Main.java to take arguments from daq

--- subset/security/brute_test/src/main/java/Main.java
+++ subset/security/brute_test/src/main/java/Main.java
@@ -3,23 +3,17 @@ public class Main {
   public static void main(String[] args) {
 
     // telnet OR ssh AS A PROTOCOL
-	  
-//	if (args.length != 4) {
-//      throw new IllegalArgumentException(
-//          "Usage: target_ip protocol(ssh/telnet) target_port target_mac");
-//    }
-	
-//	String host = args[0];
-//    String protocol = args[1];
-//    int connectionPort = Integer.parseInt(args[2]);
-//    String macAddress = args[3];
-	
-	String host = "192.168.1.2";
-	String  protocol = "ssh";
-	int connectionPort = 22;
-	//String macAddress = "40:bd:32:f5:93:52";
-	String macAddress = "00:1a:eb:11:00:pi";
-  
+
+     if (args.length != 4) {
+      throw new IllegalArgumentException(
+      "Usage: target_ip protocol(ssh/telnet) target_port target_mac");
+    }
+
+	String host = args[0];
+    String protocol = args[1];
+    int connectionPort = Integer.parseInt(args[2]);
+    String macAddress = args[3];
+
     System.out.println("Main Started...");
     SetupTest setupTest = new SetupTest(protocol, host, connectionPort, macAddress);
   }
