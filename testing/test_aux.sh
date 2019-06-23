efe52363f189b005359e0a6ccae4ba9452e3f1f4
Trevor Pering <peringknife@google.com>
2019-06-23 13:13:15 -0700

    Adding udmi to faux test configurations

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -25,8 +25,8 @@ cat <<EOF >> local/system.conf
 fail_hook=misc/dump_network.sh
 test_config=misc/runtime_configs/long_wait
 site_path=inst/test_site
-startup_faux_1_opts=brute
-startup_faux_2_opts="nobrute expiredtls"
+startup_faux_1_opts="brute udmi"
+startup_faux_2_opts="nobrute expiredtls udmi"
 startup_faux_3_opts="tls macoui bacnet"
 EOF
 
