bffa6828734462b1d4beb4b2dcce8fc6181bba03
Trevor Pering <peringknife@google.com>
2019-09-12 10:09:26 -0700

    Reinstate firestore

--- daq/gcp.py
+++ daq/gcp.py
@@ -46,8 +46,7 @@ class GcpManager:
         self._client_name = self._parse_creds(cred_file)
         self._pubber = pubsub_v1.PublisherClient(credentials=self._credentials)
         LOGGER.info('Initialized gcp pub/sub %s:%s', self._project, self._client_name)
-        #self._firestore = self._initialize_firestore(cred_file)
-        self._firestore = None
+        self._firestore = self._initialize_firestore(cred_file)
         self._report_bucket_name = self.REPORT_BUCKET_FORMAT % self._project
         self._storage = storage.Client(project=self._project, credentials=self._credentials)
         self._ensure_report_bucket()
