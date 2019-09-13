33651beb45e1ee3f54781d1198ddba7addcf20c3
Trevor Pering <peringknife@google.com>
2019-09-13 16:14:59 -0700

    Remove whitespace

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -53,7 +53,6 @@ if [ -f $cred_file ]; then
     project_id=`jq .project_id $cred_file`
 
     cloud_file=inst/test_site/cloud_iot_config.json
-
     echo Pulling cloud iot details from $cloud_file...
     registry_id=`jq .registry_id $cloud_file`
     cloud_region=`jq .cloud_region $cloud_file`
