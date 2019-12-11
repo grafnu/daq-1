f4b9ac5ef99738978ea634c7787e228187bf7141
jhughes <jonathan.hughes@buildingsiot.com>
2019-12-11 05:09:09 +0000

    Update to java 11 for tls 1.3 support Add specific TLS version testing Add reports for each TLS version test

--- subset/security/tlstest/src/main/java/Certs.java
+++ subset/security/tlstest/src/main/java/Certs.java
@@ -1,145 +1,186 @@
+
 import java.io.IOException;
-import java.net.*;
-import java.security.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateNotYetValidException;
 import java.security.cert.X509Certificate;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.*;
 
 public class Certs {
 
-  String ipAddress = "127.0.0.1";
-  String certificateReport = "";
+  public static String ipAddress;
+  public static int port;
 
-  Report report = new Report();
-
-  public Certs(String ipAddress) {
+  public Certs(String ipAddress, int port) {
     this.ipAddress = ipAddress;
+    this.port = port;
   }
 
-  public boolean getCertificate() {
+  String certificateReport = "";
+  Report report = new Report();
+
+  public boolean testTLSVersions() throws Exception {
     try {
-      disableSslVerification();
-
-      URL url;
-
-      url = new URL(ipAddress);
-      HttpsURLConnection httpsURLConnection;
-      httpsURLConnection = (HttpsURLConnection) url.openConnection();
-      httpsURLConnection.connect();
-      Principal peername = httpsURLConnection.getPeerPrincipal();
-      String cipher = httpsURLConnection.getCipherSuite();
-      java.security.cert.Certificate[] certificates = httpsURLConnection.getServerCertificates();
-      certificateReport += "Cipher:\n" + cipher + "\n";
-      for (java.security.cert.Certificate certificate : certificates) {
-
-        if (certificate instanceof X509Certificate) {
-          try {
-            ((X509Certificate) certificate).checkValidity();
-            certificateReport += "Certificate is active for current date.\n";
-            passTlsV3(true);
-            passX509(true);
-          } catch (CertificateExpiredException cee) {
-            certificateReport += "Certificate is expired.\n";
-            passTlsV3(false);
-            passX509(false);
-            return false;
-          } catch (CertificateNotYetValidException e) {
-            certificateReport += "Certificate not yet valid.\n";
-            passTlsV3(false);
-            passX509(false);
-            return false;
-          }
-
-          certificateReport += "\n";
-
-          certificateReport += "Certificate:\n" + certificate + "\n";
-
-        } else {
-          certificateReport += "Unknown certificate type.\n";
-          passTlsV3(false);
-          passX509(false);
-          System.err.println("Unknown certificate type: " + certificate);
-          return false;
-        }
-      }
-      return true;
-    } catch (MalformedURLException e) {
-      certificateReport += "MalformedURLException unable to connect to server.\n";
-      System.err.println("getCertificate MalformedURLException:" + e.getMessage());
-      skipTlsX509();
-      return false;
-    } catch (IOException e) {
-      certificateReport += "IOException unable to connect to server.\n";
-      System.err.println("getCertificate IOException:" + e.getMessage());
-      skipTlsX509();
-      return false;
+      boolean tlsV12 = testTLS12();
+      boolean tlsV13 = testTLS13();
+      boolean certValid = tlsV12 || tlsV13;
+      passX509(certValid);
+      return certValid;
     } finally {
       report.writeReport(certificateReport);
     }
   }
 
-  private void passTlsV3(boolean status){
-    if(status){
-      certificateReport += "RESULT pass security.tls.v3\n";
-    } else {
-      certificateReport += "RESULT fail security.tls.v3\n";
+  private boolean testTLS12() throws Exception {
+    return testTLS("1.2");
+  }
+
+  private boolean testTLS13() throws Exception {
+    return testTLS("1.3");
+  }
+
+  private boolean testTLS(String tlsVersion) throws Exception {
+    SSLSocket socket;
+    try {
+      // Attempt to open an SSL Socket at the TLS version specified
+      socket = makeSSLSocket(ipAddress, port, "TLSv" + tlsVersion);
+    } catch (IOException e) {
+      certificateReport += "IOException unable to connect to server.\n";
+      System.err.println("testTLS IOException:" + e.getMessage());
+      skipTls(tlsVersion);
+      return false;
+    }
+
+    // Validate Server Certificates While using specified TLS version
+    try {
+      Certificate[] certificates = getServerCertificates(socket);
+      boolean certValid = validateCertificates(certificates);
+      passTls(certValid, tlsVersion);
+      return certValid;
+    } catch (SSLHandshakeException e) {
+      certificateReport += "SSLHandshakeException: Unable to complete handshake\n";
+      System.err.println("SSLHandshakeException:" + e.getMessage());
+      passTls(false, tlsVersion);
+      return false;
     }
   }
 
-  private void passX509(boolean status){
-    if(status){
+  private boolean validateCertificates(Certificate[] certificates) {
+    for (Certificate certificate : certificates) {
+
+      if (certificate instanceof X509Certificate) {
+        try {
+          ((X509Certificate) certificate).checkValidity();
+          certificateReport += "Certificate is active for current date.\n";
+          certificateReport += "\n";
+          certificateReport += "Certificate:\n" + certificate + "\n";
+          return true;
+
+        } catch (CertificateExpiredException cee) {
+          certificateReport += "Certificate is expired.\n";
+          return false;
+        } catch (CertificateNotYetValidException e) {
+          certificateReport += "Certificate not yet valid.\n";
+          return false;
+        }
+
+      } else {
+        certificateReport += "Unknown certificate type.\n";
+        System.err.println("Unknown certificate type: " + certificate);
+        return false;
+      }
+    }
+    return false;
+  }
+
+  private void passX509(boolean status) {
+    if (status) {
       certificateReport += "RESULT pass security.x509\n";
     } else {
-      certificateReport += "RESULT fail security.x509\n";      
+      certificateReport += "RESULT fail security.x509\n";
     }
   }
 
-  private void skipTlsX509(){
-    certificateReport += "RESULT skip security.tls.v3\n";
+  private void passTls(boolean status, String tlsVersion) {
+    if (status) {
+      certificateReport += "RESULT pass security.tls.v" + tlsVersion + "\n";
+    } else {
+      certificateReport += "RESULT fail security.tls.v" + tlsVersion + "\n";
+    }
+  }
+
+  private void skipTls(String tlsVersion) {
+    certificateReport += "RESULT skip security.tls.v" + tlsVersion + "\n";
+  }
+
+  private void skipTlsX509() {
     certificateReport += "RESULT skip security.x509\n";
   }
 
-  private static void disableSslVerification() {
-    try {
-      // Create a trust manager that does not validate certificate chains
-      TrustManager[] trustAllCerts =
-          new TrustManager[] {
-            new X509TrustManager() {
-              public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
-              }
+  /**
+   * Creates a trust manager to accept all certificates. This is required since we need this test to
+   * be able to connect to any device and can't know anything about the certificates before hand.
+   *
+   * @return A valid TrustManager which accepts all valid X509Certificates
+   */
+  private TrustManager[] trustAllManager() {
+    // Create a trust manager that does not validate certificate chains
+    return new TrustManager[] {
+      new X509TrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+          return null;
+        }
 
-              public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
 
-              public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-            }
-          };
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+      }
+    };
+  }
 
-      // Install the all-trusting trust manager
-      SSLContext sslContext = SSLContext.getInstance("SSL");
-      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-      HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+  /**
+   * Attemps to complete the SSL handshake and retrieve the Server Certificates. Server certificates
+   * in this context refers to the device being tested.
+   *
+   * @param socket The SSLSocket which connects to the device for testing
+   * @throws Exception
+   */
+  private Certificate[] getServerCertificates(SSLSocket socket) throws IOException {
+    socket.startHandshake();
+    return socket.getSession().getPeerCertificates();
+  }
 
-      // Create all-trusting host name verifier
-      HostnameVerifier allHostsValid =
-          new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) {
-              return true;
-            }
-          };
+  /**
+   * @param host This is the host IP address of the device being tested
+   * @param port This is teh Port of the SSL connection for the device being tested.
+   * @param protocol The SSL protocol to be tested.
+   * @return SSLSocket which supports only the SSL protocol defined.
+   * @throws Exception
+   */
+  private SSLSocket makeSSLSocket(String host, int port, String protocol)
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    SSLSocketFactory factory = makeSSLFactory(trustAllManager(), protocol);
 
-      // Install the all-trusting host verifier
-      HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-    } catch (NoSuchAlgorithmException e) {
-      System.err.println("disableSslVerification NoSuchAlgorithmException:" + e.getMessage());
-    } catch (KeyManagementException e) {
-      System.err.println("disableSslVerification KeyManagementException:" + e.getMessage());
-    }
+    SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+    socket.setEnabledProtocols(new String[] {protocol});
+    return socket;
+  }
+
+  /**
+   * Create an SSLSocketFactory with the defined trust manager and protocol
+   *
+   * @param trustManager TrustManager to be used in the SSLContext
+   * @param protocol The SSL protocol to be used in the SSLContext
+   * @return An initialized SSLSocketFactory with SSLContext defined by input parameters
+   * @throws Exception
+   */
+  private SSLSocketFactory makeSSLFactory(TrustManager[] trustManager, String protocol)
+      throws NoSuchAlgorithmException, KeyManagementException {
+    // Install the all-trusting trust manager
+    SSLContext sslContext = SSLContext.getInstance(protocol);
+    sslContext.init(null, trustManager, new java.security.SecureRandom());
+    return sslContext.getSocketFactory();
   }
 }
