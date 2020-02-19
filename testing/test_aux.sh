0ac0d1e58fb00dbf1c76fa42da8871d36e9e13c3
em-redstone <edzel.monteverde@excelredstone.com>
2020-02-19 15:22:03 +0000

    sed statement now removes packet log lines

--- testing/test_aux.sh
+++ testing/test_aux.sh
@@ -137,7 +137,7 @@ echo done with docker logs
 # from comparison
 # This won't necessarily work in 2020, for example...
 function redact {
-    sed -e '/^%%.*/d' \ # Remove packet log lines that lead to diffs on spaces.
+    sed -e '/^%%.*/d' \
 	-e 's/\s*%%.*//' \
         -e 's/2020-.*T.*Z/XXX/' \
         -e 's/2020-.*00:00/XXX/' \
